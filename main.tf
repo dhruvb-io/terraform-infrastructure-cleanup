02253ef7b3a9a9a91c89075cb59c664168a0cac2
iamdhruvbansal <dhruv.bansal0098@gmail.com>
2026-03-07 18:33:59 +0530

    Terraform With CICD

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
     region = var.region
 }
 
+terraform {
+    backend "s3" {
+        bucket = "var.bucket"
+        key = "terraform.tfstate"
+        region = "var.region"
+        encrypt = true
+    }
+}
+
 resource "aws_instance" "mul_env_instance" {
     ami = var.ami
     instance_type = var.instance_type
