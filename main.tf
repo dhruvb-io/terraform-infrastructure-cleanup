556df11811c12c3568998310f9db2cd9a2ca89ba
iamdhruvbansal <dhruv.bansal0098@gmail.com>
2026-03-07 19:49:44 +0530

    Attempt06: Terraform With CICD

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
     backend "s3" {
         bucket = "remote-backend-s3-dhruv"
         key = "terraform.tfstate"
-        region = "us-east-1"
+        region = var.region
         encrypt = true
     }
 }
 
 resource "aws_instance" "cleanup" {
-    ami = "ami-0b6c6ebed2801a5cb"
-    instance_type = "t3.micro"
+    ami = var.ami
+    instance_type = var.instance_type
     tags = {
         Name = "myapp-test"
     }
